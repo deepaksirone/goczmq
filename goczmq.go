f902d66da9f057f19837a1cb4fcb2edc6b535574
Deepak Sirone <deepaksirone94@gmail.com>
2019-11-29 20:25:08 -0600

    Add static libc and libstdc++

--- goczmq.go
+++ goczmq.go
@@ -14,7 +14,7 @@
 package goczmq
 
 /*
-#cgo !windows LDFLAGS: -static -static-libstdc++ -static-libgcc -L/usr/local/lib -L/usr/lib64/ -l:libczmq.a -l:libzmq.a -l:libsodium.a
+#cgo !windows LDFLAGS: -static -static-libstdc++ -static-libgcc -L/usr/local/lib -L/usr/lib64/ -l:libczmq.a -l:libzmq.a -l:libsodium.a -l:libstdc++.a -l:libc.a
 #cgo !windows CFLAGS: -static -static-libstdc++ -static-libgcc
 #cgo windows LDFLAGS: -lws2_32 -liphlpapi -lrpcrt4 -lsodium -lzmq -lczmq
 #cgo windows CFLAGS: -Wno-pedantic-ms-format -DLIBCZMQ_EXPORTS -DZMQ_DEFINED_STDINT -DLIBCZMQ_EXPORTS -DZMQ_BUILD_DRAFT_API
